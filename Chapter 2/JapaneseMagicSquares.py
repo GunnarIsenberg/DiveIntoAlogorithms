59a60fba117075e1e3d49baaa1a6d9200db06e3e
GunnarIsenberg <52142814+GunnarIsenberg@users.noreply.github.com>
2022-05-20 13:26:30 -0700

    Update JapaneseMagicSquares.py

--- Chapter 2/JapaneseMagicSquares.py
+++ Chapter 2/JapaneseMagicSquares.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 def verifysquare(square):
@@ -25,8 +26,16 @@ def printsquare(square):
         print(format_row.format(label, *row))
 
 
-def rule1(x, n):
-    return(((x + n) % n**2))
+def rule1(x, n, upright):
+    return((x + ((-1)**upright) * n) % n**2)
+
+
+def rule2(x, n, upleft):
+    return((x + ((-1)**upleft * (-n + 1))) % n**2)
+
+
+def rule3(x, n, upleft):
+    return((x + ((-1)**upleft * (-n + 1))) % n**2)
 
 
 # This piece defines the size of the sqare we want to make
@@ -43,3 +52,53 @@ square[center_i + 1][center_j] = 1
 square[center_i - 1][center_j] = n**2
 square[center_i][center_j + 1] = n**2 + 1 - n
 square[center_i][center_j - 1] = n
+
+center_i = math.floor(n/2)
+center_j = math.floor(n/2)
+
+entry_i = center_i
+entry_j = center_j
+where_we_can_go = ['up_left', 'up_right', 'down_left', 'down_right']
+where_to_go = random.choice(where_we_can_go)
+
+if(where_to_go == 'up_right'):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j], n, True)
+
+if(where_to_go == 'down_left'):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j - 1
+    square[new_entry_i][new_entry_j] = rule1(
+        square[entry_i][entry_j], n, False)
+
+if(where_to_go == 'up_left'):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j - 1
+    square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, True)
+
+if(where_to_go == 'down_right'):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule2(
+        square[entry_i][entry_j], n, False)
+
+if(where_to_go == 'up_left' and (entry_i + entry_j) == (n)):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j - 1
+    square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j], n, True)
+if(where_to_go == 'down_right' and (entry_i + entry_j) == (n-2)):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule3(
+        square[entry_i][entry_j], n, False)
+
+where_we_can_go = []
+if(entry_i < (n - 1) and entry_j < (n - 1)):
+    where_we_can_go.append('down_right')
+if(entry_i < (n - 1) and entry_j > 0):
+    where_we_can_go.append('down_left')
+if(entry_i > 0 and entry_j < (n - 1)):
+    where_we_can_go.append('up_right')
+if(entry_i > 0 and entry_j > 0):
+    where_we_can_go.append('up_left')
